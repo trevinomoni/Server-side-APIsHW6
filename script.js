fd50d9ebdbec22d4f22ce4bf68fff1d31b5536c6
Monica Trevino <mtrevinom7@gmail.com>
2020-08-27 22:23:45 -0500

    final upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const api = {
+    key: "7609ce29a23cbbf1f6ea2a86a28a5d97"
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchbox.value);
+        console.log(searchbox.value);
+    }
+}
+
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(weather => {
+            return weather.json();
+        }).then(displayResults);
+}
+
+function displayResults(weather) {
+    console.group(weather);
+}
